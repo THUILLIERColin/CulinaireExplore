b392f39719492560f314f77b1159c0d440ccb6a3
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-08 17:43:39 +0200

    wip : Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <>
-      <h1> Hello World ! </h1>
+      <main className="bg-neutral">
+        <Navbar />
+        <div className="container mx-auto p-4">
+          <h1 className="text-3xl text-center text-corvette">
+            Hello, World!
+          </h1>
+          <p className="text-center text-corvette">
+            This is a simple page with a Tailwind CSS Navbar.
+          </p>
+        </div>
+      </main>
     </>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,82 @@
+import logo from "../assets/logo.png";
+import ToogleTheme from "./ToogleTheme";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="navbar bg-neutral">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a>À propos</a>
+              </li>
+              <li>
+                <a>Parent</a>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost text-xl">
+            <img src={logo} alt="Logo" className="w-10 h-10 rounded-lg" />
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>À propos</a>
+            </li>
+            <li>
+              <details>
+                <summary>Parent</summary>
+                <ul className="p-2">
+                  <li>
+                    <a>Submenu 1</a>
+                  </li>
+                  <li>
+                    <a>Submenu 2</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <ToogleTheme />
+        </div>
+      </div>
+    </nav>
+  );
+}
