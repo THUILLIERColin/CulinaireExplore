840ef5c9f7f4c44cf8b751d83cc2467ceab6a880
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-09 22:28:20 +0200

    fix: Refractor TermsOfSale

--- src/pages/TermsOfSale.jsx
+++ src/pages/TermsOfSale.jsx
@@ -2,40 +2,93 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 
 export default function TermsOfSale() {
-    return (
-        <>
-        <Navbar />
-        <div className="terms-container text-neutral">
+  return (
+    <>
+      <Navbar />
+      <div className="terms-container text-neutral">
         <h1 className="text-4xl font-bold text-center">Conditions de Vente</h1>
-            <p className="text-center font-lovelace text-lg max-w-4xl mx-auto my-10"
-            >En vous inscrivant pour des prestations de voyage sur le site Culinaire Explore, vous reconnaissez avoir pris connaissance des conditions générales de vente, des conditions particulières de vente et du descriptif du voyage choisi.</p>
-            
-            <h2 className="text-2xl ml-5">Préambule</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 ml-7">
-                Culinaire Explore est une agence de voyages culinaires, immatriculée au registre des opérateurs de voyages et de séjours Atout France. Notre société a développé un site Internet accessible à l’adresse URL « www.culinaire-explore.com » qui permet de consulter nos offres de services de voyages, notamment les forfaits touristiques, prestations d’hébergement, location de voiture, assurances voyages. L’acheteur reconnaît avoir été informé et avoir accepté que les offres de voyages soient régies par les présentes conditions de vente.</p>
-            
-            <h2 className="text-2xl ml-5">Article 1 : Champ d&apos;application et définitions</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Les présentes Conditions Générales de Vente s&apos;appliquent à l&apos;ensemble des produits et services proposés sur le site www.culinaire-explore.com. Elles sont accessibles à tout moment sur le site et peuvent être obtenues sur simple demande auprès de nos conseillers.</p>
+        <p className="text-center font-lovelace text-lg max-w-4xl mx-auto my-10">
+          En vous inscrivant pour des prestations de voyage sur le site
+          Culinaire Explore, vous reconnaissez avoir pris connaissance des
+          conditions générales de vente, des conditions particulières de vente
+          et du descriptif du voyage choisi.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 2 : Réservation d&apos;un voyage</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">La réservation de tout voyage ou prestation se fait directement sur le site. Toutes les étapes nécessaires à la réservation sont précisées sur le site www.culinaire-explore.com dans les sections appropriées.</p>
+        <h2 className="text-2xl ml-5">Préambule</h2>
+        <p className="font-lovelace text-lg max-w-full my-10 ml-7">
+          Culinaire Explore est une agence de voyages culinaires, immatriculée
+          au registre des opérateurs de voyages et de séjours Atout France.
+          Notre société a développé un site Internet accessible à l’adresse URL
+          « www.culinaire-explore.com » qui permet de consulter nos offres de
+          services de voyages, notamment les forfaits touristiques, prestations
+          d’hébergement, location de voiture, assurances voyages. L’acheteur
+          reconnaît avoir été informé et avoir accepté que les offres de voyages
+          soient régies par les présentes conditions de vente.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 3 : Prix et modalités de révision</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Les prix sont indiqués en euros, toutes taxes comprises. Ils peuvent être révisés en fonction des variations des frais de transport, notamment du coût du carburant ou d&apos;autres taxes liées aux prestations offertes.</p>
+        <h2 className="text-2xl ml-5">
+          Article 1 : Champ d&apos;application et définitions
+        </h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Les présentes Conditions Générales de Vente s&apos;appliquent à
+          l&apos;ensemble des produits et services proposés sur le site
+          www.culinaire-explore.com. Elles sont accessibles à tout moment sur le
+          site et peuvent être obtenues sur simple demande auprès de nos
+          conseillers.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 4 : Conditions de paiement</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Les paiements peuvent être effectués par carte bancaire ou virement bancaire, selon les indications fournies lors du processus de réservation. Une confirmation de paiement est envoyée à l&apos;acheteur par e-mail immédiatement après la transaction.</p>
+        <h2 className="text-2xl ml-5">
+          Article 2 : Réservation d&apos;un voyage
+        </h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          La réservation de tout voyage ou prestation se fait directement sur le
+          site. Toutes les étapes nécessaires à la réservation sont précisées
+          sur le site www.culinaire-explore.com dans les sections appropriées.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 5 : Annulation et modification</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Les conditions d&apos;annulation et de modification de votre réservation sont détaillées dans cette section. Il est important de noter que certaines réservations peuvent être non remboursables ou soumises à des frais d&apos;annulation spécifiques.</p>
+        <h2 className="text-2xl ml-5">
+          Article 3 : Prix et modalités de révision
+        </h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Les prix sont indiqués en euros, toutes taxes comprises. Ils peuvent
+          être révisés en fonction des variations des frais de transport,
+          notamment du coût du carburant ou d&apos;autres taxes liées aux
+          prestations offertes.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 6 : Responsabilités</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Culinaire Explore ne peut être tenu responsable des annulations ou modifications de voyage dues à des événements extérieurs tels que grèves, conditions climatiques, etc.</p>
+        <h2 className="text-2xl ml-5">Article 4 : Conditions de paiement</h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Les paiements peuvent être effectués par carte bancaire ou virement
+          bancaire, selon les indications fournies lors du processus de
+          réservation. Une confirmation de paiement est envoyée à
+          l&apos;acheteur par e-mail immédiatement après la transaction.
+        </p>
 
-            <h2 className="text-2xl ml-5">Article 7 : Réclamations</h2>
-            <p className="font-lovelace text-lg max-w-full my-10 mx-7">Toute réclamation relative à un voyage doit être formulée par écrit et envoyée à notre service clientèle dans les délais spécifiés dans nos conditions générales de vente.</p>
-        </div>
-        <Footer />
-        </>
-    );
+        <h2 className="text-2xl ml-5">
+          Article 5 : Annulation et modification
+        </h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Les conditions d&apos;annulation et de modification de votre
+          réservation sont détaillées dans cette section. Il est important de
+          noter que certaines réservations peuvent être non remboursables ou
+          soumises à des frais d&apos;annulation spécifiques.
+        </p>
+
+        <h2 className="text-2xl ml-5">Article 6 : Responsabilités</h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Culinaire Explore ne peut être tenu responsable des annulations ou
+          modifications de voyage dues à des événements extérieurs tels que
+          grèves, conditions climatiques, etc.
+        </p>
+
+        <h2 className="text-2xl ml-5">Article 7 : Réclamations</h2>
+        <p className="font-lovelace text-lg max-w-full my-10 mx-7">
+          Toute réclamation relative à un voyage doit être formulée par écrit et
+          envoyée à notre service clientèle dans les délais spécifiés dans nos
+          conditions générales de vente.
+        </p>
+      </div>
+      <Footer />
+    </>
+  );
 }
