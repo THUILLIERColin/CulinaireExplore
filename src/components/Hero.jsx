79d2d7a4ca02e1cb71a0d5998f9d4117ee1f8dbb
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-09 12:00:59 +0200

    Update Hero.jsx
    Update font

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,9 +7,9 @@ export default function Hero() {
       <div className="hero min-w-screen min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
             <div className="text-left">
-                <h1 className="text-5xl font-bold text-neutral mr-10">It&apos;s time to travel!</h1>
-                <p className="py-6 text-neutral">Direction le <img src={airplane} alt="Avion" className="w-10 h-10 inline-block" /> plaisir</p>
-                <button className="btn btn-primary">Reservez d&egrave;s maintenant !</button>
+                <h1 className="text-5xl font-aileron text-neutral mr-10">It&apos;s time to travel!</h1>
+                <p className="py-6 text-neutral font-lovelace">Direction le <img src={airplane} alt="Avion" className="w-10 h-10 inline-block" /> plaisir</p>
+                <button className="btn btn-primary font-poppins">Reservez d&egrave;s maintenant !</button>
             </div>
             <Carousel />
         </div>
