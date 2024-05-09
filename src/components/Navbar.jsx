14c33e2b51c0afbcc37334ebae9a040e7a2ed7ae
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-09 14:28:08 +0200

    Add link in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,48 +29,54 @@ export default function Navbar() {
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 font-poppins rounded-box w-52"
             >
               <li>
-                <a>À propos</a>
+                <Link to="/i-want-to-travel">Je veux voyager</Link>
               </li>
               <li>
-                <a>Parent</a>
+                <a>Qui sommes nous ?</a>
                 <ul className="p-2">
                   <li>
-                    <a>Submenu 1</a>
+                    <Link to="/concept">Notre concept</Link>
                   </li>
                   <li>
-                    <a>Submenu 2</a>
+                    <Link to="/team">Nos &eacute;quipe</Link>
                   </li>
                 </ul>
               </li>
               <li>
-                <Link to={"/contact"}>Contact</Link>
+                <Link to="/how-it-works">Comment &ccedil;a marche ?</Link>
+              </li>
+              <li>
+                <Link to="/contact">Nous contacter</Link>
               </li>
             </ul>
           </div>
-          <Link to={"/"} className="btn btn-ghost text-xl">
+          <Link to="/" className="btn btn-ghost text-xl">
             <img src={logo} alt="Logo" className="w-10 h-10 rounded-lg" />
           </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 font-poppins">
             <li>
-              <Link to="/about">À propos</Link>
+              <Link to="/i-want-to-travel">Je veux voyager</Link>
             </li>
             <li>
               <details>
-                <summary>Parent</summary>
+                <summary>Qui sommes nous ?</summary>
                 <ul className="p-2">
                   <li>
-                    <a>Submenu 1</a>
+                    <Link to="/concept">Notre concept</Link>
                   </li>
                   <li>
-                    <a>Submenu 2</a>
+                    <Link to="/team">Nos &eacute;quipe</Link>
                   </li>
                 </ul>
               </details>
             </li>
             <li>
-            <Link to={"/contact"}>Contact</Link>
+              <Link to="/how-it-works">Comment &ccedil;a marche ?</Link>
+            </li>
+            <li>
+              <Link to="/contact">Nous contacter</Link>
             </li>
           </ul>
         </div>
