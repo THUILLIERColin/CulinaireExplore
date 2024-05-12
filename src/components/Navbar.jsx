0ce43ee7b87128e2c967e9f94155688f8dc4af1f
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-12 17:28:21 +0200

    chore: Update Navbar component to close details on link click

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import PropType from "prop-types";
-import logo from "../assets/img/logo.png"
+import logo from "../assets/img/logo.png";
 import ToogleTheme from "./ToogleTheme";
 
 Navbar.propTypes = {
@@ -8,9 +8,17 @@ Navbar.propTypes = {
 };
 
 export default function Navbar() {
+  // Fonction pour fermer <details>
+  const closeDetails = (e) => {
+    // Trouve le parent <details> le plus proche et le ferme
+    const details = e.target.closest('details');
+    if (details) {
+      details.open = false;
+    }
+  };
 
   return (
-    <nav className="text-neutral">
+    <nav className="text-neutral relative z-50">
       <div className="navbar bg-base-200 border-b">
         <div className="navbar-start">
           <div className="dropdown">
@@ -30,34 +38,6 @@ export default function Navbar() {
                 />
               </svg>
             </div>
-            <ul
-              tabIndex={0}
-              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 font-poppins rounded-box w-52"
-            >
-              <li>
-                <Link to="/i-want-to-travel">Je veux voyager</Link>
-              </li>
-              <li>
-                <a>Qui sommes nous ?</a>
-                <ul className="p-2">
-                  <li>
-                    <Link to="/concept">Notre concept</Link>
-                  </li>
-                  <li>
-                    <Link to="/team">Notre &eacute;quipe</Link>
-                  </li>
-                  <li>
-                    <Link to="/commitment">Nos engagements</Link>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <Link to="/how-it-works">Comment &ccedil;a marche ?</Link>
-              </li>
-              <li>
-                <Link to="/contact">Nous contacter</Link>
-              </li>
-            </ul>
           </div>
           <Link to="/" className="btn btn-ghost text-xl">
             <img src={logo} alt="Logo" className="w-10 h-10 rounded-lg" />
@@ -73,13 +53,13 @@ export default function Navbar() {
                 <summary>Qui sommes nous ?</summary>
                 <ul className="p-2">
                   <li>
-                    <Link to="/concept">Notre concept</Link>
+                    <Link to="/concept" onClick={closeDetails}>Notre concept</Link>
                   </li>
                   <li>
-                    <Link to="/team">Notre &eacute;quipe</Link>
+                    <Link to="/team" onClick={closeDetails}>Notre &eacute;quipe</Link>
                   </li>
                   <li>
-                    <Link to="/commitment">Nos engagements</Link>
+                    <Link to="/commitment" onClick={closeDetails}>Nos engagements</Link>
                   </li>
                 </ul>
               </details>
