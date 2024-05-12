800104438dc7b1cdebf6c1eb798be4acdf22c065
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-12 16:46:35 +0200

    chore: Update Contact page layout and content

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,43 +1,75 @@
-import { PhoneIcon, MapIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import {
+  PhoneIcon,
+  MapPinIcon,
+  EnvelopeIcon,
+} from "@heroicons/react/24/outline";
 
 export default function Contact() {
   return (
-    <>    <div className="bg-base-200 m-10 py-10 px-5 md:px-10 rounded-2xl shadow-inner">
-      <div className="max-w-6xl mx-auto">
-        <h1 className="text-3xl font-bold text-center">Contact Us</h1>
-        <p className="text-center text-gray-600 mt-2 mb-10">Get in touch and let us know how we can help.</p>
-        
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-10 text-center">
-          {/* Address Card */}
-          <div className="flex flex-col items-center p-5 shadow-lg rounded-lg bg-base-100">
-            <MapIcon className="h-10 w-10 text-orange-500" />
-            <h2 className="font-semibold text-lg mt-2">Address</h2>
-            <p className="text-gray-600 mt-1">#1, Junaid Plaza, Thane - Shil Road, Mumbra - 400612</p>
-            <button className="mt-4 text-orange-500 hover:text-orange-600">View on map →</button>
-          </div>
-          
-          {/* Contact Card */}
-          <div className="flex flex-col items-center p-5 shadow-lg rounded-lg">
-            <PhoneIcon className="h-10 w-10 text-orange-500" />
-            <h2 className="font-semibold text-lg mt-2">Contact</h2>
-            <p className="text-gray-600">+91 (996) 788 1994</p>
-            <p className="text-gray-600">+91 (996) 788 1999</p>
-          </div>
-          
-          {/* Email Card */}
-          <div className="flex flex-col items-center p-5 shadow-lg rounded-lg">
-            <EnvelopeIcon className="h-10 w-10 text-orange-500" />
-            <h2 className="font-semibold text-lg mt-2">Email</h2>
-            <p className="text-gray-600">support@mailerbakery.com</p>
-            <p className="text-gray-600">contact@mailerbakery.com</p>
-          </div>
-        </div>
+    <>
+      <div className="bg-base-200 m-10 py-10 px-5 md:px-10 rounded-2xl shadow-inner">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-4xl font-bold font-aileron text-neutral text-center">Contactez-nous</h1>
+          <p className="text-md text-center text-neutral font-lovelace mt-2 mb-10">
+            Vous avez <strong>une question</strong> ? Une proposition de <strong>partenariat</strong> ? 
+            <br />
+            N&apos;hésitez pas à nous contacter par email, nous vous répondrons le plus vite possible.
+          </p>
 
-        <div className="text-center text-gray-600 mt-10">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae.</p>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-10 text-center">
+            {/* Phone Card */}
+            <div className="flex flex-col items-center p-5 shadow-lg rounded-lg bg-base-100">
+              <PhoneIcon className="h-10 w-10 text-neutral" />
+              <h2 className="font-bold font-aileron text-neutral text-lg mt-2">
+                T&eacute;l&eacute;phone
+              </h2>
+              <p className="text-primary mt-2">
+                <a href="tel:+33 3 54 33 27 02" className="link link-hover">
+                  (+33) 3 54 33 27 02
+                </a>
+              </p>
+            </div>
+
+            {/* Email Card */}
+            <div className="flex flex-col items-center p-5 shadow-lg rounded-lg bg-base-100">
+              <EnvelopeIcon className="h-10 w-10 text-neutral" />
+              <h2 className="font-bold font-aileron text-neutral text-lg mt-2">
+                Email
+              </h2>
+              <p className="text-primary mt-2">
+                <a
+                  href="mailto:contact@culinaire.fr"
+                  className="link link-hover"
+                >
+                  contact@culinaire-explore.fr
+                </a>{" "}
+              </p>
+            </div>
+
+            {/* Address Card */}
+            <div className="flex flex-col items-center p-5 shadow-lg rounded-lg bg-base-100">
+              <MapPinIcon className="h-10 w-10 text-neutral" />
+              <h2 className="font-bold font-aileron text-neutral text-lg mt-2">
+                Adresse
+              </h2>
+              <p className="text-primary mt-1">
+                8 Rue des Cinq-Piquets, Nancy, 54000, France
+              </p>
+            </div>
+          </div>
+
+          <div className="text-center text-sm text-neutral font-poppins mt-10">
+            <p>
+              Vous avez un <strong>projet de voyage particulier</strong> ? 
+              Une <strong>demande spécifique</strong> ?
+              <br/> 
+              Vous pouvez nous écrire par email, prendre RDV pour que l&apos;on
+              vous appelle (téléphone) ou bien formuler une demande de devis
+              ci-dessous :
+            </p>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
