22115c091ea60c554b2ba7fc3de5e3352171556d
THUILLIERColin <colin.thuillier@gmail.com>
2024-05-13 11:06:52 +0200

    feat: implement individual image loading state

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,21 @@
 import airplane from "../assets/svg/airplane.svg";
 import { Link, useLocation } from "react-router-dom";
 import SuccessModal from "./SuccessModal";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
   const success = queryParams.get("success");
+  const [imagesLoaded, setImagesLoaded] = useState({
+    image1: false,
+    image2: false,
+    image3: false,
+    image4: false,
+    image5: false,
+    image6: false,
+    image7: false
+  });
 
   useEffect(() => {
     if (success) {
@@ -18,6 +27,10 @@ export default function Hero() {
       }, 3000);
     }
   }, [success]);
+
+  const handleImageLoaded = (imageKey) => {
+    setImagesLoaded(prevState => ({ ...prevState, [imageKey]: true }));
+  };
 
   return (
     <>
@@ -55,56 +68,70 @@ export default function Hero() {
                     <div className="flex items-center space-x-6 lg:space-x-8">
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-100">
+                        {!imagesLoaded.image1 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1659333377868-308305e24aa8?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Restaurant Grecque"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image1")}
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        {!imagesLoaded.image2 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1603360946369-dc9bb6258143?q=80&w=2535&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Nourriture Grecque"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image2")}
                           />
                         </div>
                       </div>
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden opacity-0 sm:opacity-100 rounded-lg">
+                        {!imagesLoaded.image3 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1589704379319-3b83010f661e?q=80&w=2061&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Restaurant Espagnol"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image3")}
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        {!imagesLoaded.image4 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://plus.unsplash.com/premium_photo-1663855531676-3fcab15776af?q=80&w=2565&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Paella Espagnole"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image4")}
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
+                        {!imagesLoaded.image5 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1711141179475-b415da29c8b1?q=80&w=2582&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Rue de Madrid"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image5")}
                           />
                         </div>
                       </div>
                       <div className="grid flex-shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8">
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
+                          {!imagesLoaded.image1 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1671369527952-2caec36dee50?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Nourriture Portugaise"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image6")}
                           />
                         </div>
                         <div className="h-64 w-44 overflow-hidden rounded-lg">
+                          {!imagesLoaded.image1 && <div className="skeleton h-64 w-44"></div>}
                           <img
                             src="https://images.unsplash.com/photo-1505819244306-ef53954f9648?q=80&w=2499&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                             alt="Rue Portugaise"
                             className="h-full w-full object-cover object-center"
+                            onLoad={() => handleImageLoaded("image7")}
                           />
                         </div>
                       </div>
