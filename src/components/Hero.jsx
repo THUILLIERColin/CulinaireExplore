efdc5e3ad20a9ac8a7a4af912c686e66cb542780
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-13 23:21:36 +0200

    chore: Add fade-left animation to Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -61,6 +61,7 @@ export default function Hero() {
               <div className="mt-10">
                 <div
                   aria-hidden="true"
+                  data-aos="fade-left"
                   className="pointer-events-none lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
                 >
                   <div className="absolute transform sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
