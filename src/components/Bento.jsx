1bf3db0e1c428c15cf36ab927d109c6cc5c5fb2a
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-14 15:34:53 +0200

    chore: Add Bento component for displaying page

---
+++ src/components/Bento.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+Bento.propTypes = {
+    dataAos: PropTypes.string.isRequired,
+    };
+
+export default function Bento({ dataAos }) {
+    return (
+      <section id="bento" data-aos={dataAos} >
+        <div className="py-4 px-2 mx-auto max-w-screen-xl sm:py-4 lg:px-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 h-full">
+            <div className="col-span-2 sm:col-span-1 md:col-span-2 h-auto md:h-full flex flex-col">
+              <Link
+                to="/i-want-to-travel"
+                className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 flex-grow"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1510812431401-41d2bd2722f3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Wines"
+                  className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">
+                  Je veux voyager
+                </h3>
+              </Link>
+            </div>
+            <div className="col-span-2 sm:col-span-1 md:col-span-2">
+              <a
+                href=""
+                className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40 mb-4"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1504675099198-7023dd85f5a3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Gin"
+                  className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">
+                  Gin
+                </h3>
+              </a>
+              <a
+                href=""
+                className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 pt-40"
+              >
+                <img
+                  src="https://images.unsplash.com/photo-1571104508999-893933ded431?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                  alt="Vodka and Whiskey"
+                  className="absolute inset-0 h-full w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out"
+                />
+                <div className="absolute inset-0 bg-gradient-to-b from-gray-900/25 to-gray-900/5"></div>
+                <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">
+                  Vodka and Whiskey
+                </h3>
+              </a>
+            </div>
+          </div>
+        </div>
+      </section>
+    );
+  }
+  
