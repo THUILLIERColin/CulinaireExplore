58d51eb9ecbf11d02369edbc20ccd5e2f637fbf0
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-05-21 14:34:58 +0200

    chore: Update Concept page with Culinaire Explore description

--- src/pages/Concept/Concept.jsx
+++ src/pages/Concept/Concept.jsx
@@ -1,7 +1,36 @@
-export default function Concept() {
+const Concept = () => {
   return (
     <>
-    Concept
+      <div className="container mx-auto px-4 py-12">
+        <h1 className="text-3xl font-bold text-center text-gray-800">Le Concept de Culinaire Explore</h1>
+        <div className="mt-8 text-gray-600 text-lg">
+          <p className="mb-4">
+            Nous sommes <strong>Rose et Valentine</strong>, deux amies passionnées. Rose est une voyageuse intrépide tandis que Valentine
+            raffole des bons plats de sa grand-mère. Ensemble, nous avons décidé d&apos;allier nos deux passions pour créer quelque chose d&apos;unique :
+            une expérience qui ravira vos yeux et vos papilles.
+          </p>
+          <p className="mb-4">
+            Notre entreprise vous propose de voyager à travers les paysages et le patrimoine culturel, en explorant
+            les délices culinaires européennes. Que vous soyez un amateur de cuisine aventureux ou un voyageur passionné en quête de nouvelles
+            expériences, nous vous promettons un voyage sensoriel inoubliable.
+          </p>
+          <h2 className="text-2xl font-semibold mt-6">Notre plus :</h2>
+          <p className="mb-4">
+            Culinaire Explore c&apos;est un accompagnement personnalisé et une création de projet unique pour chaque client. C&apos;est accessible à tous,
+            les parcours et restaurations pourront être réalisés en fonction de différents critères.
+          </p>
+          <ul className="list-disc list-inside">
+            <li>Un accompagnement du premier rendez-vous au retour de votre voyage.</li>
+            <li>La possibilité de réaliser des demandes particulières.</li>
+            <li>Des offres spéciales pour les familles nombreuses, les groupes d&apos;amis, les enfants et personnes âgées.</li>
+          </ul>
+          <p className="mt-6 text-center text-xl font-semibold">
+            Vous voilà prêt à embarquer dans cette aventure gastronomique et culturelle !
+          </p>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
+
+export default Concept
