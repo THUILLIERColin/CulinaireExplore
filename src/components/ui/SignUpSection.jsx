0d09e95b5bf150501e7a115e247dd90fa653e057
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-06-20 18:52:11 +0200

    fix: like rest of site

--- src/components/ui/SignUpSection.jsx
+++ src/components/ui/SignUpSection.jsx
@@ -3,24 +3,33 @@ export default function SignUpSection() {
     <div className="bg-base-200 py-12 px-4">
       <div className="max-w-7xl mx-auto flex flex-wrap md:flex-nowrap justify-between items-start">
         <div className="md:w-1/2">
-          <h2 className="text-3xl font-bold text-gray-800">
+          <h2 className="text-3xl font-bold text-neutral font-aileron">
             Rejoignez la communauté de Culinaire Explore !
           </h2>
-          <p className="mt-2 text-xl text-gray-600">
-            Créez votre compte pour une expérience personnalisée et accédez à des
-            offres exclusives.
+          <p className="mt-2 text-xl text-neutral font-lovelace">
+            Créez votre compte pour une expérience personnalisée et accédez à
+            des offres exclusives.
           </p>
-          <button className="mt-6 px-6 py-2 bg-blue-500 text-white font-semibold rounded-md shadow hover:bg-blue-600 transition-colors">
+          <button className="inline-block rounded-md border border-transparent bg-primary px-8 py-3 text-center font-medium text-white hover:opacity-80 m-5">
             Créez votre compte gratuitement
           </button>
         </div>
         <div className="md:w-1/2 mt-8 md:mt-0 md:pl-12">
-          <ul className="list-disc space-y-2 text-left text-gray-600">
-            <li>📨 Offres exclusives de voyages directement dans votre boîte mail.</li>
-            <li>📣 Promotions spéciales pendant le mois de votre anniversaire.</li>
+          <ul className="list-none space-y-2 text-left text-neutral font-lovelace">
+            <li className="pb-2">
+              📨 Offres exclusives de voyages directement dans votre boîte mail.
+            </li>
+            <li className="pb-2">
+              📣 Promotions spéciales pendant le mois de votre anniversaire.
+            </li>
             <li>📚 Accès privilégié à nos guides de voyage détaillés.</li>
-            <li>📉 Réductions et offres spéciales tout au long de l&apos;année.</li>
-            <li>🗓️ La possibilité de planifier et sauvegarder vos itinéraires personnalisés.</li>
+            <li className="pb-2">
+              📉 Réductions et offres spéciales tout au long de l&apos;année.
+            </li>
+            <li className="pb-2">
+              🗓️ La possibilité de planifier et sauvegarder vos itinéraires
+              personnalisés.
+            </li>
           </ul>
         </div>
       </div>
