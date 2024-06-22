016d6ab5a54ae05df107f5b8041c8deb5fad64bf
THUILLIERColin <90446137+THUILLIERColin@users.noreply.github.com>
2024-06-22 18:28:04 +0200

    chore: Update home page with sign up and partners sections

--- src/components/ui/SignUpSection.jsx
+++ src/components/ui/SignUpSection.jsx
@@ -1,37 +1,35 @@
 export default function SignUpSection() {
   return (
-    <div className="bg-base-200 py-12 px-4">
-      <div className="max-w-7xl mx-auto flex flex-wrap md:flex-nowrap justify-between items-start">
-        <div className="md:w-1/2">
-          <h2 className="text-3xl font-bold text-neutral font-aileron">
-            Rejoignez la communauté de Culinaire Explore !
-          </h2>
-          <p className="mt-2 text-xl text-neutral font-lovelace">
-            Créez votre compte pour une expérience personnalisée et accédez à
-            des offres exclusives.
-          </p>
-          <button className="inline-block rounded-md border border-transparent bg-primary px-8 py-3 text-center font-medium text-white hover:opacity-80 m-5">
-            Créez votre compte gratuitement
-          </button>
-        </div>
-        <div className="md:w-1/2 mt-8 md:mt-0 md:pl-12">
-          <ul className="list-none space-y-2 text-left text-neutral font-lovelace">
-            <li className="pb-2">
-              📨 Offres exclusives de voyages directement dans votre boîte mail.
-            </li>
-            <li className="pb-2">
-              📣 Promotions spéciales pendant le mois de votre anniversaire.
-            </li>
-            <li>📚 Accès privilégié à nos guides de voyage détaillés.</li>
-            <li className="pb-2">
-              📉 Réductions et offres spéciales tout au long de l&apos;année.
-            </li>
-            <li className="pb-2">
-              🗓️ La possibilité de planifier et sauvegarder vos itinéraires
-              personnalisés.
-            </li>
-          </ul>
-        </div>
+    <div className="max-w-7xl mx-auto flex flex-wrap md:flex-nowrap justify-between items-start">
+      <div className="md:w-1/2">
+        <h2 className="text-3xl font-bold text-neutral font-aileron">
+          Rejoignez la communauté de Culinaire Explore !
+        </h2>
+        <p className="mt-2 text-xl text-neutral font-lovelace">
+          Créez votre compte pour une expérience personnalisée et accédez à des
+          offres exclusives.
+        </p>
+        <button className="inline-block rounded-md border border-transparent bg-primary px-8 py-3 text-center font-medium text-white hover:opacity-80 m-5">
+          Créez votre compte gratuitement
+        </button>
+      </div>
+      <div className="md:w-1/2 mt-8 md:mt-0 md:pl-12">
+        <ul className="list-none space-y-2 text-left text-neutral font-lovelace">
+          <li className="pb-2">
+            📨 Offres exclusives de voyages directement dans votre boîte mail.
+          </li>
+          <li className="pb-2">
+            📣 Promotions spéciales pendant le mois de votre anniversaire.
+          </li>
+          <li>📚 Accès privilégié à nos guides de voyage détaillés.</li>
+          <li className="pb-2">
+            📉 Réductions et offres spéciales tout au long de l&apos;année.
+          </li>
+          <li className="pb-2">
+            🗓️ La possibilité de planifier et sauvegarder vos itinéraires
+            personnalisés.
+          </li>
+        </ul>
       </div>
     </div>
   );
--- src/pages/Home/PartnersSection.jsx
+++ src/pages/Home/PartnersSection.jsx
@@ -1,34 +1,52 @@
 export default function PartnersSection() {
-    const partners = [
-      { name: "Lufthansa", link: "https://www.lufthansa.com/fr/fr/homepage", imgSrc: "https://companieslogo.com/img/orig/LHA.DE-909aa95f.png?t=1635288732" },
-      { name: "Air France", link: "https://wwws.airfrance.fr/", imgSrc: "https://companieslogo.com/img/orig/AF.PA-b92a6da2.png?t=1671948758" },
-      { name: "Ryanair", link: "https://www.ryanair.com/fr/fr", imgSrc: "https://companieslogo.com/img/orig/RYAAY-66486679.png?t=1634625582" },
-      { name: "TheFork", link: "https://www.theforkmanager.com/fr-fr",imgSrc: "https://images.squarespace-cdn.com/content/v1/659eed6da17a5024e85fcfb5/883dbd9b-05a2-4cd8-b92b-cc095dd4bf78/TheFork-Icon.png" }
-    ];
-  
-    return (
-      <section aria-labelledby="partners" className="bg-base py-12 px-4">
-        <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
-          <div className="order-2 md:order-1">
-            <h2 id="partners" className="text-4xl font-bold text-neutral text-left">
-              Nos partenaires
-            </h2>
-            <p className="mt-4 font-lovelace text-xl text-neutral mx-auto">
-              Nous sommes actuellement en cours de négociation avec des partenaires de qualité pour vous offrir des expériences culinaires uniques.
-            </p>
+  const partners = [
+    {
+      name: "Lufthansa",
+      link: "https://www.lufthansa.com/fr/fr/homepage",
+      imgSrc:
+        "https://companieslogo.com/img/orig/LHA.DE-909aa95f.png?t=1635288732",
+    },
+    {
+      name: "Air France",
+      link: "https://wwws.airfrance.fr/",
+      imgSrc:
+        "https://companieslogo.com/img/orig/AF.PA-b92a6da2.png?t=1671948758",
+    },
+    {
+      name: "Ryanair",
+      link: "https://www.ryanair.com/fr/fr",
+      imgSrc:
+        "https://companieslogo.com/img/orig/RYAAY-66486679.png?t=1634625582",
+    },
+    {
+      name: "TheFork",
+      link: "https://www.theforkmanager.com/fr-fr",
+      imgSrc:
+        "https://images.squarespace-cdn.com/content/v1/659eed6da17a5024e85fcfb5/883dbd9b-05a2-4cd8-b92b-cc095dd4bf78/TheFork-Icon.png",
+    },
+  ];
+
+  return (
+    <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+      <div className="order-2 md:order-1">
+        <h2 id="partners" className="text-4xl font-bold text-neutral text-left">
+          Nos partenaires
+        </h2>
+        <p className="mt-4 font-lovelace text-xl text-neutral mx-auto">
+          Nous sommes actuellement en cours de négociation avec des partenaires
+          de qualité pour vous offrir des expériences culinaires uniques.
+        </p>
+      </div>
+      <div className="order-1 md:order-2 flex flex-wrap justify-center gap-4 md:justify-end">
+        {partners.map(({ name, link, imgSrc }, i) => (
+          <div key={i} className="p-4 bg-base-100 shadow-inner rounded-xl">
+            <a href={link} target="_blank" rel="noopener noreferrer">
+              <img src={imgSrc} alt="Partner" className="h-16 w-16 mb-2" />
+              <span className="block text-center text-neutral">{name}</span>
+            </a>
           </div>
-          <div className="order-1 md:order-2 flex flex-wrap justify-center gap-4 md:justify-end">
-            {partners.map(({ name, link, imgSrc }, i) => (
-              <div key={i} className="p-4 bg-base-200 shadow-inner rounded-xl">
-                <a href={link} target="_blank" rel="noopener noreferrer">
-                  <img src={imgSrc} alt="Partner" className="h-16 w-16 mb-2" />
-                  <span className="block text-center text-neutral">{name}</span>
-                </a>
-              </div>
-            ))}
-          </div>
-        </div>
-      </section>
-    );
-  }
-  
+        ))}
+      </div>
+    </div>
+  );
+}
